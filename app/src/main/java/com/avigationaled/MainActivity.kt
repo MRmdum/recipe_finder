02348a7832826d71c31185bf82599bbb6f9f45f5
Pierre Olivier COUDERT <pierreolivier.coudert@laposte.net>
2023-11-30 11:57:18 +0100

    Initial commit

---
+++ app/src/main/java/com/avigationaled/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.avigationaled
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+    }
+
+
+}
---
+++ app/src/main/java/com/avigationaled/mainfrag.kt
@@ -0,0 +1,71 @@
+package com.avigationaled
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [mainfrag.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class mainfrag : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_mainfrag, container, false)
+
+        val buttonFragmentA: Button? = view?.findViewById(R.id.supabutton)
+        buttonFragmentA?.setOnClickListener {
+            findNavController().navigate(R.id.page1)
+        }
+        return view
+    }
+
+    //findViewById<Button>(R.id.supabutton).setOnClickListener {
+    //    Log.d("BUTTONS", "User tapped the Supabutton")
+    //}
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment mainfrag.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            mainfrag().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
