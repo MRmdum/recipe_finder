8de0ec9edcd77aceb2d22325631f24fa20d2fb82
Manon Goffinet <95745958+Man-G0@users.noreply.github.com>
2023-12-17 23:35:44 +0100

    debut filter

--- app/src/main/java/com/avigationaled/mainfrag.kt
+++ app/src/main/java/com/avigationaled/mainfrag.kt
@@ -1,19 +1,17 @@
 package com.avigationaled
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.SearchView
-import android.widget.TextView
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.launch
@@ -44,8 +42,10 @@ class mainfrag : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
 
+    ): View? {
+        var actualSearch :String = ""
+        val list2meal:List<Meal>?=null
         lifecycleScope.launch{
             val url = "https://www.themealdb.com/api/json/v1/1/search.php?s="
             HttpKtorClient().fromHttpGetWriteDB(url,requireContext())
@@ -64,73 +64,125 @@ class mainfrag : Fragment() {
 
         }
 
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.recipy_search, container, false)
+
+        val spinner = view.findViewById<Spinner>(R.id.spinner)
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.optionsSpinner,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                // Handle item selection here
+                val selectedOption = spinner.selectedItem.toString()
+                if (selectedOption =="Recipies"){
+                    updateSearchRecipicies(actualSearch)
+                }else{
+                    upadateSearchIngredients(actualSearch,list2meal)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                spinner.setSelection(1)
+            }
+        }
 
         var searchView :SearchView = view.findViewById(R.id.searchV_Research)
         searchView.clearFocus()
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
             override fun onQueryTextSubmit(query: String): Boolean {
+                actualSearch = query
                 return true
             }
 
             override fun onQueryTextChange(newText: String): Boolean {
-                lifecycleScope.launch{
-                    try {
-                        val list2meal = UserRepository(requireContext()).getMealByName(newText)
-                        if (list2meal != null) {
-                            val recyclerView: RecyclerView? = view?.findViewById(R.id.recyclerView)
-                            for(meal in list2meal){
-                                Log.d("DB result",meal.toString())
-
-                            }
-                            if (recyclerView != null) {
-                                recyclerView.layoutManager = LinearLayoutManager(requireContext())
-                                recyclerView.adapter = CustomAdapter(list2meal,
-                                    requireContext(), this@mainfrag)
-                            }
-                        }
-                    }catch (e:Exception){Log.d("ErrorReadSQLite",e.toString())}
+                actualSearch = newText
+                val selectedOption = spinner.selectedItem.toString()
+                if (selectedOption =="Recipies"){
+                    updateSearchRecipicies(actualSearch)
+                }else{
+                    upadateSearchIngredients(actualSearch,list2meal)
                 }
                 return false
             }
 
+
+
         })
-
-        /*
-        val text2recherche: TextView? = view?.findViewById(R.id.searchV_Research)
-        text2recherche?.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-            override fun afterTextChanged(editable: Editable?) {
-                // This method is called to notify you that the characters within `editable` have changed.
-                val text = editable.toString()
-                // Do something with the new text
-                lifecycleScope.launch{
-                    try {
-                        val list2meal = UserRepository(requireContext()).getMealByName(text)
-                        if (list2meal != null) {
-                            for(meal in list2meal){
-                                Log.d("DB result",meal.toString())
-                            }
-                        }
-                    }catch (e:Exception){Log.d("ErrorReadSQLite",e.toString())}
-                }
-            }
-        })*/
-
-
-
 
         return view
     }
 
-    //findViewById<Button>(R.id.supabutton).setOnClickListener {
-    //    Log.d("BUTTONS", "User tapped the Supabutton")
-    //}
+    private fun updateSearchRecipicies(search:String){
+        lifecycleScope.launch{
+            try {
+                val list2meal = UserRepository(requireContext()).getMealByName(search)
+                if (list2meal != null) {
+                    val recyclerView: RecyclerView? = view?.findViewById(R.id.recyclerView)
+                    for(meal in list2meal){
+                        Log.d("DB result",meal.toString())
 
+                    }
+                    if (recyclerView != null) {
+                        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+                        recyclerView.adapter = CustomAdapter(list2meal,
+                            requireContext(), this@mainfrag)
+                    }
+                }
+            }catch (e:Exception){Log.d("ErrorReadSQLite",e.toString())}
+        }
+    }
+    private fun upadateSearchIngredients(search:String, list2meal:List<Meal>?){
+        if (list2meal != null) {
+            var list2Ingredient: MutableList<Meal>? = mutableListOf()
+            for (meal in list2meal) {
+                // Create a list of ingredients for the current meal
+                val ingredients = listOf(
+                    meal.strIngredient1, meal.strIngredient2, meal.strIngredient3, meal.strIngredient4,
+                    meal.strIngredient5, meal.strIngredient6, meal.strIngredient7, meal.strIngredient8,
+                    meal.strIngredient9, meal.strIngredient10, meal.strIngredient11, meal.strIngredient12,
+                    meal.strIngredient13, meal.strIngredient14, meal.strIngredient15, meal.strIngredient16,
+                    meal.strIngredient17, meal.strIngredient18, meal.strIngredient19, meal.strIngredient20
+                )
+
+                // Join the list of ingredients into a single string
+                val joinedIngr = ingredients.joinToString(" / ")
+
+                // Check if the specified ingredient is present in the current meal
+                if (ingredients.contains(search)) {
+                    list2Ingredient?.add(meal)
+
+                }
+            }
+            lifecycleScope.launch{
+                try {
+
+                    if (list2Ingredient != null) {
+                        val recyclerView: RecyclerView? = view?.findViewById(R.id.recyclerView)
+                        for(meal in list2Ingredient){
+                            Log.d("DB result",meal.toString())
+
+                        }
+                        if (recyclerView != null) {
+                            recyclerView.layoutManager = LinearLayoutManager(requireContext())
+                            recyclerView.adapter = CustomAdapter(list2Ingredient,
+                                requireContext(), this@mainfrag)
+                        }
+                    }
+                }catch (e:Exception){Log.d("ErrorReadSQLite",e.toString())}
+            }
+
+        }
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
