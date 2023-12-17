4cf2e9e220bac9dd537b50ee179ab473b4778fca
Mathis0507 <26080@student.dorset-college.ie>
2023-12-17 15:11:33 +0100

    Test

--- app/src/main/java/com/avigationaled/UserDB.kt
+++ app/src/main/java/com/avigationaled/UserDB.kt
@@ -40,8 +40,6 @@ interface UserDao {
     @Query("SELECT * FROM user_table WHERE username = :username")
     fun getUserName(username: String): UserDB?
 
-    @Query("INSERT INTO user_table (username,password) VALUES (:username,:password)")
-    fun  createUser(username:String, password:String): UserDB?
 }
 
 @Database(entities = [UserDB::class,Meal::class], version = 1)
